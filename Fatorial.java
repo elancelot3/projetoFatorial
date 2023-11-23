9a521cfe13dafddf1648ee07f0d4fb826d6de382
Evandro Rocha Almeida <elancelot3@gmail.com>
2023-11-22 21:12:37 -0300

    metodo main

--- Fatorial.java
+++ Fatorial.java
@@ -1,8 +1,4 @@
 public class Fatorial{
-   public static void main(String[] args){
-      system.out.println(calcularFatorial(5));
-   }
-
    public static int calcularFatorial(int n) {
       int resultado = 1;
       for (int i = 0; i < n; i++)
